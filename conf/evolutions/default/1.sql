ae5347d8c98181ea6d286a5454db9f5e8e2b6a37
Shreyas Hande <shreyas@everwell.org>
2023-01-18 20:48:12 +0530

    initial commit

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,15 @@
+# --- !Ups
+
+CREATE TABLE `book_store`.`books`
+(
+    `id` BIGINT NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(25) NOT NULL ,
+    `author` VARCHAR(25) NOT NULL ,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+
+# --- !Downs
+
+DROP TABLE IF EXISTS books;
+
